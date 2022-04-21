faec34c7a39dd787a5eda58e912b9bfbb48ded4e
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-04-21 09:37:48 +0700

    modif title program 1, pertemuan1 program 2

---
+++ pertemuan1/program2.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mencetak String</title>
+</head>
+<body>
+    <pre>
+        <?php 
+            echo "Mencetak Menggunakan Echo \n";
+            echo "== ROBIN SETIYAWAN == \n";
+            print "Mencetak Mengguanakn Print \n";
+            print "== Namaku : ROBIN SETIAYWAN == \n";
+        ?>
+    </pre>
+</body>
+</html>
