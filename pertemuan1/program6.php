2664569cad874ff46092722a76eae1be0a3b8ecd
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-04-21 09:59:27 +0700

    pertemuan1, program6'

---
+++ pertemuan1/program6.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komentar Type Casting</title>
+</head>
+<body>
+    <?php
+        $my_float = 7.5; // memberikan nilai pecahan ke variabel $my_float
+        echo $my_float; // menampilkan data yang terkandugn dalam $my_float
+        echo "<br>";
+        echo (int) $my_float; // hasil type casting dalam bentuk bilangan integer
+    ?>
+</body>
+</html>
