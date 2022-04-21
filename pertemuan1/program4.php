5bf13010f8dfa6c17fe837fb1662c2338c67fa01
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-04-21 09:47:30 +0700

    pertemuan1, program4

---
+++ pertemuan1/program4.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komentar - C ++ Style</title>
+</head>
+<body>
+    <?php 
+        // ini menggunakan skrip larihan komentar
+        // menggunakan double slash //
+        echo "Mencoba komentar PHP menggunkaan C++ Style."
+    ?>
+</body>
+</html>
