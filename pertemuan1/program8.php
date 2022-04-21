2cd3a522f38bbecb510a0e5c60d00738dcff47aa
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-04-21 10:29:46 +0700

    pertemuan1, program8

---
+++ pertemuan1/program8.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komentar variabel gettype()</title>
+</head>
+<body>
+    Bilangan positif : <?= +5 ?>
+    <br/>
+    Bilangan Negatif : <?= -5 ?>
+    <br/>
+    5 + 3 = <? 5 + 3 ?>
+    <br/>
+    5 - 3 = <? 5 - 3 ?>
+    <br/>
+    5 * 3 = <? 5 * 3 ?>
+    <br/>
+    5 / 3 = <? 5 / 3 ?>
+    <br/>
+    5 % 3 = <? 5 % 3 ?>
+    <br/>
+    5 * (-10) - (-3) = <? 5 * (-10) - (-3) ?>
+</body>
+</html>
