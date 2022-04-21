f155e264693bd3fa2ac15b760bbeae1ac3bd52d3
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-04-21 09:45:02 +0700

    Pertemuan1, progra3

---
+++ pertemuan1/program3.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komentar - Unix Sheel <Style></Style></title>
+</head>
+<body>
+    <?php 
+        #ini adalah skrip latihan komentar
+        # Mengguanakn hash #
+        echo "Mencoba komentar PHP menggunakan Unix Shell Style.";
+        echo "<br>";
+        echo "---------------------------------------------------------------------";
+    ?>
+</body>
+</html>
