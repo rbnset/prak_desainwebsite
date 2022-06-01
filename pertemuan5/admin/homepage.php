40ecf1a1ae433408394c00018cf8e5bf9b45f0f4
rbnsetiyawan <rbn.setiyawan@gmail.com>
2022-06-01 23:08:46 +0700

    Pertemuan 5

---
+++ pertemuan5/admin/homepage.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Membuat Form Login Dengan PHP MySQL | Tutorial</title>
+</head>
+
+<body>
+    <h1>Membuat Form Login Dengan PHP MySQL | Tutorial</h1>
+    <h3>LOGIN BERHASIL !!!</h3>
+    <br />
+    <br />
+    <a href="./logout.php">Log out</a>
+</body>
+
+</html>
--- pertemuan5/login.php
+++ pertemuan5/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include './koneksi.php';
+include 'koneksi.php';
 
 // menangkap data yang dikirim dari form login
 $username = $_POST['username'];
@@ -15,7 +15,7 @@ $cek = mysqli_num_rows($data);
 if ($cek > 0) {
     $_SESSION['username'] = $username;
     $_SESSION['status'] = "login";
-    header("location:admin/index.php");
+    header("location:admin/homepage.php");
 } else {
     header("location:index.php?pesan=gagal");
 }
